52ab2c5c93f8999089c92620408e84b964e3297e
patrickdk <patrickdk@patrickdk.com>
2025-08-25 08:47:07 -0400

    fix pass assignment

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -17,7 +17,7 @@ func main() {
         user := os.Getenv("USERNAME")
 	pass := os.Getenv("REDIS_PASSWORD")
 	if len(pass) == 0 {
-		pass := os.Getenv("PASSWORD")
+		pass = os.Getenv("PASSWORD")
 	}
 	client := redis.NewClient(&redis.Options{ Addr: host, Username: user, Password: pass, })
 	role, err := client.Do(ctx,"role").Result()
